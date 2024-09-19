d663422e50f4e72d8a9dfe39699795927ffee429
Sefa Aycicek <saycicek@ipucubilisim.com.tr>
2024-09-19 09:43:31 +0300

    Initial Commit

---
+++ TurkcellApi3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TurkcellApi3
+//
+//  Created by Sefa Aycicek on 19.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
